8ee1ce8b3abd278e4b30a67aec309d387bdb1a92
Javier <javier.sardinas@gmail.com>
2022-06-22 09:51:29 -0500

    Implement signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,8 +3,9 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
+        console.log('signup request');
         const userData = await User.create(req.body);
-
+        console.log(userData)
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
