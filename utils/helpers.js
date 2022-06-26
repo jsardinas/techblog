121a96c44e8f476a17c5f0d19098c1ae66b1a819
Javier <javier.sardinas@gmail.com>
2022-06-25 20:10:16 -0500

    Fix date formatter

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,10 @@
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
-      //return date.toLocaleDateString();
-      return '01/01/1970';
+      if(date)
+        return date.toLocaleDateString();
+      else
+        return '';
     }
   };
   
